b0d102a3f3b92c5081968231db40398201e3c55a
Annisa Nuri Nabila <73707695+annisann@users.noreply.github.com>
2023-01-02 10:15:08 +0700

    function to calculate tax from sales

--- ts__getSalesTax.ts
+++ ts__getSalesTax.ts
@@ -1,3 +1,22 @@
-function getSalesTax(price: number, tax: number) : any {
-    
+function getSalesTax(price: any, tax: any) : any {
+    let totalSales = price + (price * (tax/100))
+    try {
+        if (typeof price !== 'number' && typeof tax !== 'number'){
+            throw new Error("Price dan pajak harus dalam angka");
+        } else if (typeof tax !== 'number') {
+            throw new Error("Pajak harus dalam angka");
+        } else if (typeof price !== 'number'){
+            throw new Error("Price harus dalam angka");
+        } else {
+            return totalSales
+        }
+    } catch (error: any) {
+        return error.message
+    }
 }
+
+console.log(getSalesTax("a", 1))
+console.log(getSalesTax(1, "angka"))
+console.log(getSalesTax("harga", "tax"))
+
+console.log(getSalesTax(4500, 5))
