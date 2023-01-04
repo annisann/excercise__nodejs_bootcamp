12049f8745c5c717d17e697dd3102634a845b51b
annisann <ennoza@student.ub.ac.id>
2023-01-04 17:05:03 +0700

    CRUD SQL with Express

---
+++ backend__express/express.js
@@ -0,0 +1,55 @@
+// Init express
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+// Init database using PostgreSQL
+const Pool = require('pg').Pool
+const pool = new Pool(
+    {
+        host: "localhost",
+        user: "postgres",
+        password: " ",
+        database: "adventurework",
+        port: 5432
+    }
+)
+console.log(pool)
+
+const PORT = process.env.port || 4141
+
+app.get(
+    `/`, // url 
+    function(request, response) {
+        response.end("Hello")
+    })
+
+app.listen(
+    PORT,
+    () => {
+        console.log(`Server listening on port ${PORT}`)
+    }
+)
+
+// Buat crud with sql raw native table department
+app.get(
+    "/departments",
+    (request, response) => {
+        /**
+         ** Call `pool` for querying
+         * Param 1: SQL Query
+         * Param 2: Binding Parameter
+         * Param 3: Result Callback
+         */
+        pool.query(
+            "SELECT DepartmentID, Name, GroupName FROM hr.d",
+            [],
+            (error, result) => {
+                if (error) {
+                    throw new Error("Can't fetch result.")
+                }
+                response.status(200)
+                    .json(result.rows) // Result shows in JSON
+            }
+        )
+    })
