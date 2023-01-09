e90c11e95dc7303fad9373c2bf92e432a41dec80
annisann <ennoza@student.ub.ac.id>
2023-01-09 14:10:32 +0700

    add jwt for authorization

--- backend__express/controllers/auth/login.js
+++ backend__express/controllers/auth/login.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken"
 import personController from "../personController"
 import bcrypt from "bcrypt"
 
@@ -5,7 +6,7 @@ const logIn = async(request, response) => {
     const input = request.body
 
     // find person by username
-    personController.findUserByEmail(
+    await personController.findUserByEmail(
         (userResult) => {
             const userData = userResult[0]
             /** userResult:
@@ -18,7 +19,20 @@ const logIn = async(request, response) => {
                 // check if password match
                 const passwordMatch = bcrypt.compareSync(input.password, userData.passwordhash)
                 if (passwordMatch) {
-                    response.send(`Log In succeed! Hello ${userData.firstname}`)
+                    // delete userResult.password
+                    
+                    let token = jwt.sign(userData, process.env.SECRET_KEY, { // add jwt secretkey in .env
+                        expiresIn: '5m' // for token to expire
+                    })
+
+                    response
+                        .status(200)
+                        .json(
+                            {
+                                message:`Log In succeed! Hello ${userData.firstname}`,
+                                token: token
+                            })
+                        
                 } else {
                     response.send(`Password is not match!`)
                 }
@@ -29,4 +43,22 @@ const logIn = async(request, response) => {
         input.email)
 }
 
-export default logIn
+const checkToken = (request, response, next) => {
+    if (!request.headers.authorization) {
+        response.status(403).send(`You are not authorized`)
+    } else {
+        let token = request.headers.authorization
+
+        try {
+            jwt.verify(token, process.env.SECRET_KEY)
+            return next() // process next method
+        } catch (error) {
+            return response.status(401).json("Invalid Token")
+        }
+    }
+}
+
+export default {
+    logIn,
+    checkToken
+}
