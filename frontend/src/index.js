47363cce250525ae364fc34fc591c487d9d08aad
annisann <ennoza@student.ub.ac.id>
2023-01-18 13:19:11 +0700

    simple calculator app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,56 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import ClassComponent from './components/latihanClassComponent';
-import MyComponent from './components/classComponentWithConstructor';
-import MyFunctionalComponent from './components/functionalComponent';
-import Addition from './components/calculator/addition';
-import Substraction from './components/calculator/substraction';
-import Multiplication from './components/calculator/multiplication';
-import Division from './components/calculator/division';
-import SendName from './components/parentChildComponent/sendNameData';
+import Calculator from './components/calculatorWithProps/calculatorInterface';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//     <ClassComponent />
-//   </React.StrictMode>
-// );
-
-// root.render(
-//   <React.StrictMode>
-//     <ClassComponent />
-//   </React.StrictMode>  
-// )
-
-// root.render(
-//   <React.StrictMode>
-//     <MyComponent/>
-//   </React.StrictMode>
-// )
-
-// root.render(
-//   <React.StrictMode>
-//     <MyFunctionalComponent/>
-//   </React.StrictMode>
-// )
-
 /** Calculator */
 root.render(
   <React.StrictMode>
-    {/* <Addition/>
-    <br/>
-    <Substraction/>
-    <br/>
-    <Multiplication/>
-    <br/>
-    <Division/> */}
-    <SendName/>
+    <Calculator></Calculator>
   </React.StrictMode>
 )
 
