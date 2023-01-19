51cb1b017258484a0b2f7ab183ca5a7725cfe8a7
annisann <ennoza@student.ub.ac.id>
2023-01-19 10:45:22 +0700

    enable cors in nest

--- backend__nest/src/main.ts
+++ backend__nest/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const PORT = process.env.PORT;
   await app.listen(PORT, () => {
