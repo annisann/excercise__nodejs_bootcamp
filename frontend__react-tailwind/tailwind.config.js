07e8152f9b4b28dd22198de49b3e51a049313f58
annisann <ennoza@student.ub.ac.id>
2023-01-24 11:15:46 +0700

    init

--- frontend__react-tailwind/tailwind.config.js
+++ frontend__react-tailwind/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography')
+  ],
 }
