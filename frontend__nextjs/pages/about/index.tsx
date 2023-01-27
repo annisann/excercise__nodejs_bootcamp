0d28d0aa450e8862a8289a1703b7ad51aefe73fe
annisann <ennoza@student.ub.ac.id>
2023-01-27 17:09:37 +0700

    inline tailwind to separate file

--- frontend__nextjs/pages/about/index.tsx
+++ frontend__nextjs/pages/about/index.tsx
@@ -1,9 +1,10 @@
+import styles from "@/styles/about.module.css"
 
 export default function About() {
   return (
-    <div className='h-screen w-screen items-center flex flex-col justify-center'>
-      <h1 className="font-bold text-5xl"> about page. </h1>
-      <div className="max-w-lg">
+    <div className={styles['body-container']}>
+      <h1 className={styles.pagetitle}> about page. </h1>
+      <div className={styles['text-container']}>
         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Delectus, assumenda magnam atque facilis veniam cum id illo nostrum doloremque nulla sunt
           velit rerum quis vero rem ratione hic voluptates ipsum?
