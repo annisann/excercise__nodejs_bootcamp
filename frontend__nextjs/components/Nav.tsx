62ddabd954632b50a66970fcc1a0eb7a70f72cb4
annisann <ennoza@student.ub.ac.id>
2023-01-30 14:59:39 +0700

    styling

--- frontend__nextjs/components/Nav.tsx
+++ frontend__nextjs/components/Nav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Nav(){
     return (
-        <nav>
+        <nav className="text-center">
             <Link href='/'> home </Link>
             <Link href='/about'> about </Link>
             <Link href='/login'> login  </Link>
--- frontend__nextjs/pages/index.tsx
+++ frontend__nextjs/pages/index.tsx
@@ -1,7 +1,11 @@
+import styles from "@/styles/home.module.css"
+
 export default function Home() {
     return (
+
         <div className='h-screen w-screen items-center flex flex-col justify-center'>
-            <h1 className="font-bold text-blue-600 text-5xl"> never gonna give u up ~ </h1>
+            
+            <h1 className={styles.pagetitle}> never gonna give u up ~ </h1>
             <div className="max-w-lg">
                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Delectus, assumenda magnam atque facilis veniam cum id illo nostrum doloremque nulla sunt
